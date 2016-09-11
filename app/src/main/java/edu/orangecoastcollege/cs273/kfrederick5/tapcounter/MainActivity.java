9e8e6eb6ca96cda942fe3f7c70cc84ba67d9e6ea
kfrederick5 <tefsuncle@gmail.com>
2016-09-10 22:44:25 -0700

    Changed... things?

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tapcounter/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tapcounter/MainActivity.java
@@ -2,13 +2,21 @@ package edu.orangecoastcollege.cs273.kfrederick5.tapcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button tapButton;
+    private TextView countText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        void
+        Button = tapButton
 
 
 
