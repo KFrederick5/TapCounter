a9d3de7f861fcc68ce091f3b8a99ef29b6e3740a
Keegan Frederick <tefsuncle@hotmail.com>
2016-09-11 12:55:24 -0700

    Linked buttons and added text change to button tap.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tapcounter/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tapcounter/MainActivity.java
@@ -2,6 +2,9 @@ package edu.orangecoastcollege.cs273.kfrederick5.tapcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,30 +13,45 @@ public class MainActivity extends AppCompatActivity {
     private Button tapButton;
     private TextView countText;
 
+    //Associate controller with needed model
+    Counter myCount = new Counter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        void
-        Button = tapButton
+        tapButton = (Button) findViewById(R.id.tapButton);
+        countText = (TextView) findViewById(R.id.countText);
 
-
-
-
+        //Define listener for countText onTextChange
+        tapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countText.addTextChangedListener(countTextChangedListener);
+            }
+        });
     }
 
-    public class Counter
-    {
-        private int mCount;
-
-        public Counter()
-        {
-            mCount =0;
+    private TextWatcher countTextChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            //Nothing
         }
 
-    }
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            try {
+                myCount.addOne();
+            }
+            catch (NumberFormatException e) {
+                countText.setText("");
+            }
+        }
 
-
-
+        @Override
+        public void afterTextChanged(Editable editable) {
+            //Nothing
+        }
+    };
 }
